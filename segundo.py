f76d413a15e68a389bd6a9cef31daa544d3ffcad
Ericha Barbosa <erichataina02@gmail.com>
2025-09-29 17:32:06 -0300

    Add files via upload

---
+++ segundo.py
@@ -0,0 +1,6 @@
+n = int(input('digite um número inteiro positivo: '))
+i = 1
+while i <= n:
+    print(i)
+    i = i + 1
+print('fim do programa')
---
+++ terceiro.py
@@ -0,0 +1,8 @@
+total_geral = 0
+i=1
+while i<=3:
+     qto_acai = int(input('Informe a quantidade de açaís vendidos %d dia(s) atrás'))
+     total = qto_acai *3.00
+     print('foram vendidos rs %.2f de açaí' %total)
+     i=i+1
+print ('Foram vendidos R$ %.2f de açaí nos dias' %(total_geral,i))
